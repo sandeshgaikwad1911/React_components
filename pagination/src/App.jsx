4bcecb3d115c30d1ea902608c4ee6d159210cba7
sandesh gaikwad <sandesh.gaikwad1911@gmail.com>
2023-10-18 01:13:19 +0530

    pagination backend-driven code

--- pagination/src/App.jsx
+++ pagination/src/App.jsx
@@ -1,4 +1,4 @@
-
+/* 
 import { useEffect, useState } from 'react'
 import './App.css'
 
@@ -49,6 +49,7 @@ function App() {
 
   return (
     <div className='app'>
+      <h1 className='heading'>Pagination</h1>
         {
             products.length > 0 && (
 
@@ -79,7 +80,7 @@ function App() {
               {
                 [...Array(products.length / 10)].map((_,i)=>{
                   return(
-                    <span key={i} onClick={()=>selectPageHandler(i+1)} 
+                    <span key={i} onClick={()=>selectPageHandler(i+1)}    // i+1  bcz i start from 0
                       className={pageNo === i+1 ? "active": ""}
                     >
                       {i+1}
@@ -89,6 +90,119 @@ function App() {
               }
               <span onClick={()=>selectPageHandler(pageNo + 1)} 
                 className={pageNo < products.length/10 ? " " : 'disabled_pagination'}
+              >
+                ▶
+              </span>
+            </div>
+          )
+        }
+
+    </div>
+  )
+}
+
+export default App
+ */
+
+
+
+
+import { useEffect, useState } from 'react'
+import './App.css'
+
+function App() {
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(null)
+  const [products, setProducts] = useState([])
+
+  const [pageNo, setPageNo] = useState(1);
+
+  const [totalPages, setTotalPages] = useState(0);
+  // console.log('totalPages',totalPages)  // 10
+ 
+  const fetchProducts = async()=>{
+    try {
+      setIsLoading(true)
+      setIsError(false);
+      const res = await fetch(`https://dummyjson.com/products?limit=10&skip=${pageNo * 10 - 10}`)
+      const data = await res.json();
+      // console.log('data', data)
+  
+      if(data && data.products){
+        setProducts(data.products);
+        setTotalPages(data.total/10);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      setIsError(error.message)
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(()=>{
+    fetchProducts();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[pageNo])
+
+  const selectPageHandler = (selectedPage)=>{
+    // we need to handle ◀ and ▶ as well so
+    if(selectedPage >=1 && selectedPage <= totalPages && selectedPage != pageNo){
+      setPageNo(selectedPage)
+    }
+
+  }
+
+  if(isLoading){
+    return <h3 style={{textAlign: "center"}}>Loading...</h3>
+  }
+  if(isError){
+    return <h3 style={{textAlign: "center"}}>Error : {isError}</h3>
+  }
+
+  return (
+    <div className='app'>
+      <h1 className='heading'>Pagination</h1>
+        {
+            products.length > 0 && (
+
+              <div className='products'>
+                  {
+                    products.map((product)=>{
+                      return(
+                        <div key={product.id} className='product'>
+                          <img src={product.thumbnail} alt={product.title} />
+                          <span>{product.id}</span>
+                        </div>
+                      )
+                    })
+                  }
+              </div>
+
+            )
+        }
+
+        {
+          products.length > 0 && (
+            <div className="pagination">
+              <span onClick={()=>selectPageHandler(pageNo - 1)}
+                className={pageNo > 1 ? " " : 'disabled_pagination'}
+              >
+                ◀
+                </span>
+              {
+                [...Array(totalPages)].map((_,i)=>{
+                  return(
+                    <span key={i} onClick={()=>selectPageHandler(i+1)}    // i+1  bcz i start from 0
+                      className={pageNo === i+1 ? "active": ""}
+                    >
+                      {i+1}
+                    </span>
+                  )
+                })
+              }
+              <span onClick={()=>selectPageHandler(pageNo + 1)} 
+                className={pageNo < totalPages ? " " : 'disabled_pagination'}
               >
                 ▶
               </span>
