141610115fcb18ab69f25f60320e929c978a8c11
sandesh gaikwad <sandesh.gaikwad1911@gmail.com>
2023-11-13 14:24:16 +0530

    updated radio

--- radioButton/src/App.jsx
+++ radioButton/src/App.jsx
@@ -1,43 +1,43 @@
-import { useState } from 'react';
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const Sizes = ["Regular", "Medium", "Large"];
 
-  const options = ["Cricket", "Football", "Kabbadi", "Swimming"];
-  const days = ["Weekdays", "Weekend"]
+  const [selectedSize, setSelectedSize] = useState("Regular");
 
-  const [option, setOption] = useState("")
-  const [day, setDay] = useState("")
-  console.log('option',option)
-  console.log('day', day)
+  const handleChange = (e) => {
+    setSelectedSize(e.target.value);
+  };
 
   return (
-    <div className='app'>
-        <div className="radio">
-          {
-            options.map((option,i)=>(
-              <div key={i} className='option'>
-                <input type="radio" id="options" name='options' value={option} onChange={(e)=>setOption(e.target.value)}/>
-                <label htmlFor="options">{option}</label>
-              </div>
-            ))
-          }
-          <br />
-          {
-            days.map((day, i)=>(
-              <div key={i} className='day'>
-                <input type="radio" id='day' name='day' value={day} onChange={(e)=>setDay(e.target.value)}/>
-                <label htmlFor="day">{day}</label>
-              </div>
-            ))
-          }
-          <br />
-          <div>
-            <h4>{`lets play :- ${option} on :- ${day}`}</h4>
+    <div className="app">
+      <div className="radio">
+        <div className="radio-options">
+          <div className="single-radio">
+            <input
+              type="radio" id="regular" name="pizza-size" value="Regular" onChange={handleChange} checked={selectedSize=="Regular"}/>
+            <label htmlFor="regular">Regular</label>
+          </div>
+
+          <div className="single-radio">
+            <input type="radio" id="medium" name="pizza-size" value="Medium" onChange={handleChange} checked={selectedSize=="Medium"}/>
+            <label htmlFor="medium">Medium</label>
+          </div>
+
+          <div className="single-radio">
+            <input type="radio" id="large" name="pizza-size" value="Large" onChange={handleChange} checked={selectedSize=="Large"}/>
+            <label htmlFor="large">Large</label>
           </div>
         </div>
+
+        <div className="selected">
+          <p>selectedt size is <strong>{selectedSize}</strong></p>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
